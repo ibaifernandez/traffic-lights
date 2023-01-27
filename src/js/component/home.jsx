2d74d1a96e2b66321b7cc227b609ec3a59c0c587
Ibai Fernández <info@ibaifernandez.com>
2023-01-27 18:37:26 -0500

    Checking that Font Awesome Icons are duly installed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,13 +8,13 @@ const Home = () => {
             <img className="d-block my-3 mx-auto" src={favicon} />
             <div className="text-center mx">
                 <span className="fs-6">
-                    If Font Awesome are active you should see a hand here 👉🏽{" "}
+                    If Font Awesome are active, you should see a hand here 👉🏽{" "}
                 </span>
                 <i className="far fa-hand ms-3"></i>
                 <span className="mx-3 fs-3">Hello React!</span>
                 <i className="far fa-hand me-3"></i>
                 <span className="fs-6">
-                    👈🏽 If Font Awesome are active you should see a hand here{" "}
+                    👈🏽 If Font Awesome are active, you should see a hand here{" "}
                 </span>
             </div>
         </>
